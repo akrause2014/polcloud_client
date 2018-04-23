9ae6e8989b4dcc8da2406f798045e6e7763a771b
akrause2014 <a.krause@epcc.ed.ac.uk>
2018-04-23 10:33:27 +0100

    initial revision

---
+++ polcloud/client.py
@@ -0,0 +1,110 @@
+import os
+import requests
+from requests_toolbelt import MultipartEncoderMonitor
+
+BASE_URL = os.getenv("WEBAPP")
+if BASE_URL is None:
+    BASE_URL = 'https://webapp-amy.azurewebsites.net/'
+    # BASE_URL = 'http://localhost:5000/'
+INPUTS = BASE_URL + 'inputs'
+SPECS = BASE_URL + 'specifications'
+POOLS = BASE_URL + 'pools'
+JOBS = BASE_URL + 'jobs'
+
+class Job:
+
+    def set_user(self, token):
+        self.params = {'token': token}
+
+    def create_input(self, *input_files):
+        if not input_files:
+            r = requests.post(INPUTS, params=self.params)
+        else:
+            files = []
+            for file_name in input_files:
+                files.append(('file[]', (os.path.basename(file_name), open(file_name, 'rb'))))
+            r = requests.post(INPUTS, files=files, params=self.params)
+        r.raise_for_status()
+        self.inputs = r.text
+
+    def update_input(self, file_name, callback=None):
+
+        m = MultipartEncoderMonitor.from_fields(
+            fields={'file[]': (os.path.basename(file_name), open(file_name, 'rb'))},
+            callback=callback
+            )
+
+        r = requests.put('%s/%s' % (INPUTS, self.inputs), data=m, params=self.params, headers={'Content-Type': m.content_type})
+        r.raise_for_status()
+
+    def create_pool(self, size):
+        self.pool = Pool.create(size, params=self.params)
+
+    def set_pool(self, pool_id):
+        self.pool = Pool(id=pool_id, params=self.params)
+
+    def get_input_info(self):
+        r = requests.get('%s/%s' % (INPUTS, self.inputs), params=self.params)
+        r.raise_for_status()
+        return r.json()
+
+    def create_job_spec(self, spec):
+        r = requests.post(SPECS, json=spec, params=self.params)
+        r.raise_for_status()
+        self.spec = r.text
+
+    def get_job_spec(self):
+        r = requests.get('%s/%s' % (SPECS, self.spec), params=self.params)
+        r.raise_for_status()
+        return r.json()
+
+    def submit(self, size, wall_clock):
+        data = {
+            'wall_clock' : wall_clock,
+            'size': size,
+            'pool_name' : self.pool.id,
+            'job_spec' : self.spec
+        }
+        r = requests.post(JOBS, json=data, params=self.params)
+        self.id = r.text
+        return self.id
+
+    def get_state(self):
+        r = requests.get('%s/%s/state' % (JOBS, self.id), params=self.params)
+        r.raise_for_status()
+        return r.text
+
+    def is_complete(self):
+        r = requests.get('%s/%s/state' % (JOBS, self.id), params=self.params)
+        r.raise_for_status()
+        return r.text == 'JobState.completed'
+
+    def list_outputs(self):
+        r = requests.get('%s/%s/outputs' % (JOBS, self.id), params=self.params)
+        r.raise_for_status()
+        return r.json()
+
+class Pool:
+
+    def __init__(self, id=None, params=None):
+        self.id = id
+        self.params = params
+
+    @staticmethod
+    def create(size, params=None):
+        r = requests.post(POOLS, json={'size': size}, params=params)
+        r.raise_for_status()
+        pool = Pool(r.text, params)
+        return pool
+
+    def get_info(self):
+        r = requests.get('%s/%s' % (POOLS, self.id), params=self.params)
+        r.raise_for_status
+        return r.json()
+
+    def is_ready(self):
+        return self.get_info()['is_ready']
+
+    def delete(self):
+        r = requests.delete('%s/%s' % (POOLS, self.id), params=self.params)
+        r.raise_for_status
---
+++ polcloud/prepare_submit_polcloud.py
@@ -0,0 +1,98 @@
+import argparse
+import json
+import os
+import string
+import sys
+import time
+from .client import Job, Pool
+
+def get_gmy_filename_from_xml(xml_filename):
+    from xml.etree import ElementTree
+    tree = ElementTree.parse(xml_filename)
+    gmy_filename = tree.getroot().find('geometry/datafile').attrib['path']
+    return gmy_filename
+
+def get_callback(file_name):
+    total = os.path.getsize(file_name)
+    def progress_callback(monitor):
+        frac = monitor.bytes_read/total
+        if frac > 1:
+            frac = 1
+        sys.stdout.write('\r[{:>7.2%}] '.format(frac))
+        sys.stdout.flush()
+    return progress_callback
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Create an Azure pool and submit job')
+    parser.add_argument('xml_file', nargs='?', help='HemeLB XML input file')
+    parser.add_argument('-s', '--spec', help='existing job spec')
+    parser.add_argument('-p', '--pool', help='existing pool')
+    parser.add_argument('-t', '--token', help='user token', required=True)
+    parser.add_argument('-d', '--delete-pool', action='store_true', help='delete pool when job is complete')
+    parser.add_argument('--upload-only', action='store_true', help='upload inputs only')
+    args = parser.parse_args()
+
+    job = Job()
+    job.set_user(args.token)
+
+    if args.spec:
+        job.spec = args.spec
+        job.inputs = job.get_job_spec()['inputs']
+    else:
+        # input files
+        xml_file = args.xml_file
+        gmy_file=get_gmy_filename_from_xml(xml_file)
+
+        # upload input files in one step:
+        # job.create_input(xml_file, gmy_file)
+
+        # upload input files in several steps (if uploads are slow)
+        job.create_input()
+        print('Created input: %s' % job.inputs)
+        print('Uploading %s' % xml_file)
+        job.update_input(xml_file, get_callback(xml_file))
+        print('\nUploading %s' % gmy_file)
+        job.update_input(gmy_file, get_callback(gmy_file))
+        print('\nInput complete.')
+        print(json.dumps(job.get_input_info(), indent=4))
+
+        # create a job spec
+        with open('job_template.json') as f:
+            job_spec = json.load(f)
+        job_spec['inputs'] = job.inputs
+        for command in job_spec['commands']:
+            command['expression'] = \
+                string.Template(command['expression']).substitute(
+                    xml_file=xml_file,
+                    gmy_file=gmy_file)
+        print(json.dumps(job_spec, indent=4))
+
+        job.create_job_spec(job_spec)
+        print('Created job spec: %s' % job.spec)
+
+    if args.pool:
+        job.set_pool(args.pool)
+    else:
+        job.create_pool(size=2)
+    print("Using pool: %s" % job.pool.get_info())
+
+    print("Waiting for pool...")
+    while not job.pool.is_ready():
+        time.sleep(5)
+    print('Pool is ready: submitting job...')
+    job_id = job.submit(size=2, wall_clock='02:00')
+    print('Submitted job: %s' % job_id)
+    print('Waiting for job...')
+
+    while not job.is_complete():
+        time.sleep(5)
+
+    print('Job is complete')
+    print(json.dumps(job.list_outputs(), indent=4))
+
+    if args.delete_pool:
+        job.pool.delete()
+        print('Deleted pool')
+    else:
+        print('Pool is running: %s' % job.pool.get_info())
